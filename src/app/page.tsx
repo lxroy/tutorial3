f9cf742b0fb9efbb0490dac43b4a57e59738cfc7
alex <sanushanu92@gmail.com>
2024-03-30 14:06:18 +0545

    prisma init & adding Nextuiprovider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>This is Landing page</h1>
+      {/* <h1>This is Landing page</h1> */}
+      <br/>
+      <Button>Click me</Button>
     </div>
   );
 }
